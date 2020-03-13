c6fd0f970eb0f294a41b1de19442b66e6de82d41
Sooji Kim <yh0gogo@gmail.com>
2020-03-13 20:45:10 +0900

    Fill in Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,43 @@
-import React from "react";
-import { Link, Redirect } from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "./Header.css";
 
-const Header = () => <div>여기가 헤더다</div>;
+const Header = () => {
+  const [currentLink, setCurrentLink] = useState("project");
+  return (
+    <div className="header-container">
+      <div className="header-left">
+        <Link className="name" to="/">
+          SOOJI KIM
+        </Link>
+        <p>성장을 즐기고, 가치를 만들어 가는 개발자</p>
+      </div>
+      <div className="header-right">
+        <span
+          style={{
+            textDecoration: "none",
+          }}
+        >
+          <Link
+            className={currentLink === "project" ? "link underLine" : "link"}
+            to="/project"
+            onClick={() => setCurrentLink("project")}
+          >
+            Project
+          </Link>
+        </span>
+        <span>
+          <Link
+            className={currentLink === "me" ? "link underLine" : "link"}
+            to="/about"
+            onClick={() => setCurrentLink("me")}
+          >
+            Me
+          </Link>
+        </span>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
