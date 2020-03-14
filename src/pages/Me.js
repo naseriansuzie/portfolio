83fe1f4b6c230089a049854f7831becb30baae1f
Sooji Kim <yh0gogo@gmail.com>
2020-03-14 21:19:50 +0900

    Move style options from js to css

--- src/pages/Me.js
+++ src/pages/Me.js
@@ -1,13 +1,14 @@
 import React, { useEffect } from "react";
 import Introduction from "../components/Me/Introduction";
 import Resume from "../components/Me/Resume";
+import "./me.css";
 
 const Me = ({ setCurrentLink }) => {
   useEffect(() => {
     setCurrentLink("me");
   });
   return (
-    <div style={{ display: "flex" }}>
+    <div className="me-container">
       <Introduction />
       <Resume />
     </div>
