94ce17772cdb1553468d121593d3b0e7f1c59352
Sooji Kim <yh0gogo@gmail.com>
2020-03-14 21:29:15 +0900

    Updates

---
+++ precache-manifest.3c930edef3392f77167dbe9173d66bb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de13620c4456659c37551fbbd10a5fc0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ac7495b6fac038cad8e2",
+    "url": "/portfolio/static/css/main.6caf3aec.chunk.css"
+  },
+  {
+    "revision": "fa7ba9e0360aae0802c4",
+    "url": "/portfolio/static/js/2.98d34046.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/portfolio/static/js/2.98d34046.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac7495b6fac038cad8e2",
+    "url": "/portfolio/static/js/main.e9e92523.chunk.js"
+  },
+  {
+    "revision": "ddfc8c5eb5851c500866",
+    "url": "/portfolio/static/js/runtime-main.b754c826.js"
+  },
+  {
+    "revision": "8b65d55366f9a956647ae3ace32871e3",
+    "url": "/portfolio/static/media/sooji_england.8b65d553.png"
+  }
+]);
